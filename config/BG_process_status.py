7a61c74e5f6e4e03f651387c04109a62caeaec50
chitransh00786 <138363557+chitransh00786@users.noreply.github.com>
2025-12-07 20:17:13 +0530

    Initial commit

---
+++ config/BG_process_status.py
@@ -0,0 +1,10 @@
+class skip:
+    skip_status = False
+    stop_counter = False
+    skip_st = False
+    
+class songDownloader:
+    song_downloader = False
+    
+class playlist_manager:
+    playlist_manager = None
